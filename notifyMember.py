368e811e11b8775fe1c2436efcef348700277340
andreasma <maand@gmx.de>
2015-09-28 13:33:43 +0200

    reading the email message from a file now

--- notifyMember.py
+++ notifyMember.py
@@ -4,17 +4,16 @@ portal = api.portal.get()
 
 users = api.user.get_users()
 
-for f in users:
-  id = f.getProperty('id')
-  name = f.getProperty('fullname')
-  email = f.getProperty('email')
+f = open('message.txt')
+message = f.read()
+
+for g in users:
+  id = g.getProperty('id')
+  name = g.getProperty('fullname')
+  email = g.getProperty('email')
   
   api.portal.send_email(
     recipient=email,
     subject='New LibreOffice Extensions and Templates Website',
-    body='''We created a new LibreOffice extensions and templates 
-          website and set up an account for you. Please go to 
-          Login for the new site under ... and ask for a reset 
-          of your password. You will get an email with a link 
-          to reset your password then''',
+    body=message,
     )
